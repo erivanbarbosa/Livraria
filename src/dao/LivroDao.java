bcc435abe06ca5c5039461b67af7eff2d8b1c621
Erivan <erivan.nas@gmail.com>
2018-01-14 19:27:28 -0200

    Versao 2.0
    Inclui arquivo de lista de livros e altera o método listar da classe LivroDao para ler os livros a partir deste arquivo.

--- src/dao/LivroDao.java
+++ src/dao/LivroDao.java
@@ -1,20 +1,37 @@
 package dao;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collection;
-
+import java.util.Scanner;
 
 import modelo.Livro;
 
 public class LivroDao {
+	
+	// Nova versão do método listar, abre e lê o arquivo "livros.csv"
 	public Collection<Livro> listar() {
 		ArrayList<Livro> livros = new ArrayList<Livro>();
+		Scanner input = null;
+		try {
 
-		for (int i = 0; i < 50; i++) {
+			input = new Scanner(new File("livros.csv"));
+		} catch (FileNotFoundException e) {
+			System.err.println("Arquivo não encontrado!");
+			System.exit(1);
+		}
+
+		while (input.hasNext()) {
+			String line = input.nextLine();
+			
+			//Divide a linha lida em 3 substring, usando a virgula como caractere separador.
+			// Expressao regular ignora virgulas entre aspas.
+			String[] palavras = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
 			Livro livro = new Livro();
-			livro.setTitulo("Livro" + i);
-			livro.setAutor("Autor " + 1);
-			livro.setPreco(i * 0.80 + 20);
+			livro.setTitulo(palavras[0]);
+			livro.setAutor(palavras[1]);
+			livro.setPreco(Double.parseDouble(palavras[2]));
 
 			livros.add(livro);
 		}
